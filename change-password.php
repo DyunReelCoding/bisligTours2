90ac6dcaefc34f5f0fc3ec2eeaf9260cbdc2204e
Jonriel <iamkirito50@gmail.com>
2024-11-18 17:13:41 +0800

    changes in css path in user

--- change-password.php
+++ change-password.php
@@ -39,7 +39,7 @@ if (strlen($_SESSION['login']) == 0) {
 		<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
 		<link href="css/style.css" rel='stylesheet' type='text/css' />
-		<link href="css/custom2.css" rel='stylesheet' type='text/css' />
+		<link href="css/custom3.css" rel='stylesheet' type='text/css' />
 		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
 		<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
 		<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
